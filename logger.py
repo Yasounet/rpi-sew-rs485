e6c78181f1ddeba9487f8b85187cd9bc47b9cfd8
Yason <michal.pieprzycki@gmail.com>
2022-11-18 13:21:00 +0100

    disable most logging for overlayroot fs

--- logger.py
+++ logger.py
@@ -11,10 +11,10 @@ S7_LOG_PATH = LOG_PATH.joinpath('s7.log')
 C_LOG_PATH = LOG_PATH.joinpath('console.log')
 
 FORMAT = logging.Formatter('[%(levelname)s] - %(message)s')
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.CRITICAL
 
 
-def setup_console_logger(name, log_file, level=logging.INFO):
+def setup_console_logger(name, log_file, level=logging.INFO, log2file=False):
 
     handler = logging.StreamHandler()
     handler.setFormatter(FORMAT)
@@ -23,10 +23,10 @@ def setup_console_logger(name, log_file, level=logging.INFO):
     logger.setLevel(level)
     logger.addHandler(handler)
 
-    handler = handlers.TimedRotatingFileHandler(
-        filename=log_file, when='midnight', interval=1)
-    handler.setFormatter(FORMAT)
-    logger.addHandler(handler)
+    if log2file:
+        handler = handlers.TimedRotatingFileHandler(filename=log_file, when='midnight', interval=1)
+        handler.setFormatter(FORMAT)
+        logger.addHandler(handler)
 
     return logger
 
@@ -44,7 +44,6 @@ def setup_thread_logger(name, log_file, level=logging.INFO):
     return logger
 
 
-rs485_logger = setup_thread_logger(
-    'rs485_logger', RS_LOG_PATH, level=LOG_LEVEL)
+rs485_logger = setup_thread_logger('rs485_logger', RS_LOG_PATH, level=LOG_LEVEL)
 s7_logger = setup_thread_logger('udp_logger', S7_LOG_PATH, level=LOG_LEVEL)
 c_logger = setup_console_logger('console_logger', C_LOG_PATH, level=LOG_LEVEL)
